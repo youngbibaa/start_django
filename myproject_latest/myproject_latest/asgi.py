ae9e4077a6a13bbaa723b9239adee98ad4537d0c
Your Name <you@example.com>
2024-05-06 15:16:24 +0500

    Initial

---
+++ myproject_latest/myproject_latest/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for myproject_latest project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject_latest.settings')
+
+application = get_asgi_application()
